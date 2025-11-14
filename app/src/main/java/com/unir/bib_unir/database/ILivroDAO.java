1332702d31eeac5799693382e6e1eca6ff05e40a
Lana Cristina <lana.cristinassantos@gmail.com>
2025-11-14 16:23:30 -0400

    Primeiro commit

--- app/src/main/java/com/unir/bib_unir/database/ILivroDAO.java
+++ app/src/main/java/com/unir/bib_unir/database/ILivroDAO.java
@@ -5,7 +5,11 @@ import android.content.ContentValues;
 import java.util.List;
 
 public interface ILivroDAO {
-
-
-
+    public ContentValues pesquisarPorId(int id);
+    public List<ContentValues> pesquisarPorTitulo(String titulo);
+    public List<ContentValues> pesquisarPorAno(int ano);
+    public List<ContentValues> pesquisarPorTodos();
+    public long inserir(ContentValues cv);
+    public void alterarRegistro(int id, String titulo, String autor, int ano);
+    public void deletarRegistro(int id);
 }
